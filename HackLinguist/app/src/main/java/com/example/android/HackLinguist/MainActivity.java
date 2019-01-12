5b5a50a9d1c8b9f726b5491975f200a45fbb17e2
ShadowGits <37514710+ShadowGits@users.noreply.github.com>
2019-01-12 12:08:19 +0530

    1
    Hack Linguist

---
+++ HackLinguist/app/src/main/java/com/example/android/HackLinguist/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.HackLinguist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.android.happybirthday3.R;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+    EditText ed1,ed2;
+
+    TextView tx1;
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
